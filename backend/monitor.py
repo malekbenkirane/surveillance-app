b4d8b81bcbec2c0cb2de1e5cb97289b7d2a05699
Admin malek <adminmalek@regence.ca>
2025-04-14 14:17:38 -0400

    Initial commit

--- backend/monitor.py
+++ backend/monitor.py
@@ -1,29 +1,25 @@
-import ping3
+import subprocess
 import json
 import os
 
 # Charger la liste des serveurs depuis le fichier JSON
 file_path = os.path.join(os.path.dirname(__file__), 'server_list.json')
+with open(file_path, 'r') as f:
+    servers = json.load(f)
 
-try:
-    with open(file_path, 'r') as f:
-        servers = json.load(f)
-except FileNotFoundError:
-    print(f"Erreur : Le fichier {file_path} est introuvable.")
-    servers = {}  # Si le fichier n'est pas trouvé, initialiser un dictionnaire vide.
-except json.JSONDecodeError:
-    print(f"Erreur : Impossible de décoder le fichier {file_path}. Assurez-vous qu'il est bien formaté en JSON.")
-    servers = {}
+def ping(host):
+    # Exécute la commande ping de Windows et retourne le code de sortie
+    command = ['ping', '-n', '1', host]
+    response = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    if response.returncode == 0:
+        return True
+    else:
+        return False
 
 def check_server_status():
     server_status = {}
     for server_name, ip in servers.items():
-        try:
-            status = ping3.ping(ip)
-            if status is None:
-                server_status[server_name] = "Down (timed out)"
-            else:
-                server_status[server_name] = "Up"
-        except Exception as e:
-            server_status[server_name] = f"Down (Erreur : {str(e)})"
+        status = ping(ip)
+        server_status[server_name] = "Up" if status else "Down"
     return server_status
